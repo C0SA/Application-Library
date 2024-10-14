38e12b9da6b4fb2d5e8289bd38f0e01925bd7dc6
C0SA <cosic332@gmail.com>
2024-10-14 10:49:34 +0200

    Initial Commit : Added Spring boot repository

---
+++ ApplicationLibrary/src/test/java/com/example/ApplicationLibrary/ApplicationLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ApplicationLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
