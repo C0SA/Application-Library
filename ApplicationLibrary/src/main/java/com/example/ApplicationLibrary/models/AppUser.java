b257447c69c20c3e323e98ed779217b8b70645d1
C0SA <cosic332@gmail.com>
2024-10-14 19:35:50 +0200

    Update: Added model for Book and started on JWT
    Added model for book, but not sure in its funcionality, started 1. point in pdf that is user authentication with jwt

---
+++ ApplicationLibrary/src/main/java/com/example/ApplicationLibrary/models/AppUser.java
@@ -0,0 +1,36 @@
+package com.example.ApplicationLibrary.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.annotation.processing.Generated;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name="users")
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true)
+    private String username;
+
+
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+
+    private String role;
+
+}
---
+++ ApplicationLibrary/src/main/java/com/example/ApplicationLibrary/models/Book.java
@@ -0,0 +1,35 @@
+package com.example.ApplicationLibrary.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    private Integer publication_year;
+    private String isbn;
+    public enum status {
+        AVAILABLE,  // should be uppercase
+        UNAVAILABLE,
+        CHECKED_OUT
+        // add other statuses as needed
+    }
+
+
+}
